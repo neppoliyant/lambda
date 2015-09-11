ee7a6c142db89b8c16be4e5b937cb5ed4351d683
Shane Witbeck <shane.witbeck@jivesoftware.com>
2015-09-11 17:26:06 -0400

    round trip working for simple Jersey api gateway

--- lambda-generator/generate.sh
+++ lambda-generator/generate.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
+deleteExistingExport() {
+    rm -f /data/export/api.jar
+}
+
 generateApiJar() {
     cd /data
-    pwd
-    ls -la
     echo "calling generator: java -cp ${LAMBDA_JAR}:/data/lambda-generator/target/lambda-generator-1.0-SNAPSHOT.jar com.digitalsanctum.lambda.generator.Generator ${LAMBDA_JAR} ${LAMBDA_HANDLER} ${LAMBDA_RESOURCE_PATH} ${LAMBDA_HTTP_METHOD} ${LAMBDA_TIMEOUT}"
     java -cp ${LAMBDA_JAR}:/data/lambda-generator-1.0-SNAPSHOT.jar com.digitalsanctum.lambda.generator.Generator ${LAMBDA_JAR} ${LAMBDA_HANDLER} ${LAMBDA_RESOURCE_PATH} ${LAMBDA_HTTP_METHOD} ${LAMBDA_TIMEOUT}
     echo "generation complete!"
-    ls -la ~/.m2
 }
 
 exportApiJar() {
     echo "copying api.jar..."
-    cp /data/template/target/lambda-api-gateway-1.0-SNAPSHOT.jar /data/export/api.jar
+#    cp /data/template/target/lambda-api-gateway-1.0-SNAPSHOT.jar /data/export/api.jar
+    cp /data/template/target/lambda-api-gateway-min-1.0-SNAPSHOT.jar /data/export/api.jar
     echo "done copying"
 }
 
-env && generateApiJar && exportApiJar
+deleteExistingExport && generateApiJar && exportApiJar
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd export
-docker build -t lambda/api .
-docker run -d -p 8080:8080 --name api lambda/api
+docker build -t digitalsanctum/lambda-api .
+docker run -d -p 8080:8080 --name api digitalsanctum/lambda-api
 
